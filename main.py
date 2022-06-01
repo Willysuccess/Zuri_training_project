432497e9ec7ec06c9ab27193afbaba762ea185a6
Willisunique <willychukwu1@gmail.com>
2022-06-01 17:02:40 +0100

    First zuri github project

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ mylist = ['Rock', 'Paper', 'Scissors']
 
 computer = random.choice(mylist)
 
-user = input("Pick an option between 'Rock','Paper'and'Scissors':  ")
+while user not in mylist:
+    user = input("Pick an option between 'Rock','Paper'and'Scissors':  ")
 
 print("computer: ", computer)
 print("user: ", user)
 if computer == user:
     print("That's a tie")
-elif user not in mylist:
-    print("Error. Try again later")
 elif computer != user:
     print("You're the winner")
